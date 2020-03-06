08473b7063039f08d4c05ea52fdb1b1a8ea82e46
SaviorD7 <saviord7@gmail.com>
2020-03-06 22:48:26 +0300

    jhipster-jdl

---
+++ src/main/java/etu/project/domain/Trip.java
@@ -0,0 +1,119 @@
+package etu.project.domain;
+
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A Trip.
+ */
+@Entity
+@Table(name = "trip")
+public class Trip implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "start_time")
+    private Instant startTime;
+
+    @Column(name = "end_time")
+    private Instant endTime;
+
+    @OneToMany(mappedBy = "trip")
+    private Set<Location> locations = new HashSet<>();
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getStartTime() {
+        return startTime;
+    }
+
+    public Trip startTime(Instant startTime) {
+        this.startTime = startTime;
+        return this;
+    }
+
+    public void setStartTime(Instant startTime) {
+        this.startTime = startTime;
+    }
+
+    public Instant getEndTime() {
+        return endTime;
+    }
+
+    public Trip endTime(Instant endTime) {
+        this.endTime = endTime;
+        return this;
+    }
+
+    public void setEndTime(Instant endTime) {
+        this.endTime = endTime;
+    }
+
+    public Set<Location> getLocations() {
+        return locations;
+    }
+
+    public Trip locations(Set<Location> locations) {
+        this.locations = locations;
+        return this;
+    }
+
+    public Trip addLocation(Location location) {
+        this.locations.add(location);
+        location.setTrip(this);
+        return this;
+    }
+
+    public Trip removeLocation(Location location) {
+        this.locations.remove(location);
+        location.setTrip(null);
+        return this;
+    }
+
+    public void setLocations(Set<Location> locations) {
+        this.locations = locations;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Trip)) {
+            return false;
+        }
+        return id != null && id.equals(((Trip) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return "Trip{" +
+            "id=" + getId() +
+            ", startTime='" + getStartTime() + "'" +
+            ", endTime='" + getEndTime() + "'" +
+            "}";
+    }
+}
